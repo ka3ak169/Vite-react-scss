7ddc0fff4cc0979b250bd6095cdfedd1ed3c7b49
Aleksey Kazakov <ka3akne@mail.ru>
2024-02-26 20:10:19 +0400

    first commit

---
+++ src/jsx/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import '../scss/App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='test'>
+        Oleg
+      </div>
+    </>
+  )
+}
+
+export default App
