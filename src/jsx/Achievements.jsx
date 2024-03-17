bced539be8e70f652bd1fa7e8eb3173f22ea0406
Aleksey Kazakov <ka3akne@mail.ru>
2024-03-17 21:32:45 +0400

    начал блок с достижениями

--- src/jsx/Achievements.jsx
+++ src/jsx/Achievements.jsx
@@ -3,19 +3,40 @@ function Achievements() {
   return (
     <>
       <div className='achievements'>
-        <div>
-          <div>
-            <h2></h2>
-            <div className='banner__points'>
-              <div className='banner__point'></div>
-              <div className='banner__point'></div>
-              <div className='banner__point'></div>
-              <div className='banner__point'></div>
-            </div>
-            <p></p>
+        <div className='achievements__inscription'>
+          <h2 className='achievements__title'></h2>
+          <div className='banner__points'>
+            <div className='banner__point'></div>
+            <div className='banner__point'></div>
+            <div className='banner__point'></div>
+            <div className='banner__point'></div>
           </div>
-          <div>
-            
+          <p className='achievements__text'></p>
+        </div>
+        <div className='achievements__sheets-container'>
+          <div className='achievements__left-column'>
+            <div className='achievements__cell'>
+              <span className='achievements__number'></span>
+              <p className='achievements__cup'></p>
+            </div>
+            <div className='achievements__cell'>
+              <span className='achievements__number'></span>
+              <p className='achievements__cup'></p>
+            </div>
+            <div className='achievements__cell'>
+              <span className='achievements__number'></span>
+              <p className='achievements__cup'></p>
+            </div>
+          </div>
+          <div className='achievements__right-column'>
+          <div className='achievements__cell'>
+              <span className='achievements__number'></span>
+              <p className='achievements__cup'></p>
+            </div>
+            <div className='achievements__cell'>
+              <span className='achievements__number'></span>
+              <p className='achievements__cup'></p>
+            </div>
           </div>
         </div>
       </div>
