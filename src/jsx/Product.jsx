0d24aae86b30529ec4776bf69525981861c57421
Aleksey Kazakov <ka3akne@mail.ru>
2024-04-28 16:59:00 +0400

    реализовал логику отображения продуктов

--- src/jsx/Product.jsx
+++ src/jsx/Product.jsx
@@ -1,12 +1,30 @@
-function Product({ name, description, index }) {
-  console.log(index);
+function Product({ name, description, index, onisWideScreen }) {
+  console.log(onisWideScreen);
+  if (!onisWideScreen) {
+    return (
+      <div>
+        <h3>{name}</h3>
+        <p>{description}</p>
+      </div>
+    );
+  } else {
+    if (index % 2 === 0) {
+      return (
+        <div>
+          <h3>{name}</h3>
+          <p>{description}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>{description}</p>
+          <h3>{name}</h3>
+        </div>
+      );
+    }
+  }
 
-  return (
-    <div>
-      <h3>{name}</h3>
-      <p>{description}</p>
-    </div>
-  );
 }
 
 export default Product;
--- src/jsx/UsProduct.jsx
+++ src/jsx/UsProduct.jsx
@@ -1,11 +1,50 @@
+import { useEffect, useState } from 'react';
 import Product from './Product';
 import { products } from '../../public/data/products';
 
 function UsProduct() {
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia("(min-width: 400px)");
+
+    const handleResize = () => {
+      setIsWideScreen(mediaQueryList.matches);
+    };
+
+    mediaQueryList.addEventListener("change", handleResize);
+    handleResize(); // Вызываем функцию при начальной загрузке компонента
+
+    return () => {
+      mediaQueryList.removeEventListener("change", handleResize);
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(isWideScreen);
+  // }, [isWideScreen]);
+
+  // const renderProducts = () => {
+  //   if (isWideScreen) {
+  //     // Логика отрисовки для широких экранов
+  //     return products.map((product, index) => (
+  //       <Product key={index} name={product.name} description={product.description} index={index} onisWideScreen={isWideScreen}/>
+  //     ));
+  //   } else {
+  //     // Логика отрисовки для маленьких экранов
+  //     return products.map((product, index) => (
+  //       <div key={index}>
+  //         <h3>{product.name}</h3>
+  //       </div>
+  //     ));
+  //   }
+  // };
+
   const renderProducts = () => {
-    return products.map((product, index) => (
-      <Product key={index} name={product.name} description={product.description} index={index} />
-    ));
+      // Логика отрисовки для широких экранов
+      return products.map((product, index) => (
+        <Product key={index} name={product.name} description={product.description} index={index} onisWideScreen={isWideScreen}/>
+      ));
   };
 
   return <>{renderProducts()}</>;
