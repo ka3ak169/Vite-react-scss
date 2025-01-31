1576a7e32c2a812b0ff06f61a75748ada1b9fd22
Aleksey Kazakov <ka3akne@mail.ru>
2025-01-31 20:29:47 +0400

    Fix basename for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  // Загружаем переменные окружения
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    plugins: [react()],
-    base: '/',
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Убедитесь, что base соответствует корневому URL вашего сайта
 });
