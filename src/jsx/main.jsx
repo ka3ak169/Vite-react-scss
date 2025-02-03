220427a2f489f92d4af4d9b3b7b850f51631755d
Aleksey Kazakov <ka3akne@mail.ru>
2025-02-04 03:23:13 +0400

    изменил BrowserRouter на HashRouter

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
-import '../css/index.css'
-
-const basename = '/'; // Для кастомного домена это правильное значение
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; // Импортируем HashRouter
+import App from './App.jsx';
+import '../css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    </HashRouter>
+  </React.StrictMode>
+);
