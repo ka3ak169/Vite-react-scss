8314bebfeffeac59da4164f372c599c89f51b9a8
Aleksey Kazakov <ka3akne@mail.ru>
2025-02-15 17:36:51 +0400

    Updates

--- data/products.js
+++ data/products.js
@@ -27,27 +27,27 @@ export const products = [
 
 export const slides = [
   {
-    image: "/public/images/new_image/O1A8779.jpg",
+    image: "/images/new_image/O1A8779.jpg",
     description: "A classic strong spirit with a distinctive juniper aroma, made by distilling grain alcohol with the addition of juniper berries and various botanical components such as coriander, citrus peel, and cardamom."
   },
   {
-    image: "/images/new_image/_O1A8838new.png",
+    image: "/images/new_image/O1A8838new.png",
     description: "An exquisite Italian liqueur with a bright citrusy aroma and flavor of bergamot, made by infusing bergamot peel in neutral spirits with the addition of sugar and sometimes other citrus fruits or herbs."
   },
   {
-    image: "/images/new_image/_O1A8701new.png",
+    image: "/images/new_image/O1A8701new.png",
     description: "A fragrant liqueur with a rich flavor of black elderberries, made by infusing ripe elderberries in neutral spirits with the addition of sugar and sometimes other ingredients such as citrus or spices"
   },
   {
-    image: "/images/new_image/_O1A8939new.jpg",
+    image: "/images/new_image/O1A8939new.jpg",
     description: "A sweet, clear liqueur with a pronounced orange flavor and aroma, made by infusing dried orange peel in neutral spirits with the addition of sugar and sometimes other citrus fruits."
   },
   {
-    image: "/images/new_image/_O1A8836new.png",
+    image: "/images/new_image/O1A8836new.png",
     description: "A unique strong spirit with an exotic fruity aroma and flavor of feijoa, made by distilling fermented feijoa juice and sometimes with the addition of other fruits or botanical components."
   },
   {
-    image: "/images/new_image/_O1A8824new.png",
+    image: "/images/new_image/O1A8824new.png",
     description: "A rich and aromatic herbal liqueur, bursting with complex flavors and a velvety texture."
   },
   {
@@ -55,11 +55,11 @@ export const slides = [
     description: ""
   },
   {
-    image: "/images/new_image/_O1A8864.jpg",
+    image: "/images/new_image/O1A8864.jpg",
     description: ""
   },
   {
-    image: "/images/new_image/_O1A8707new.png",
+    image: "/images/new_image/O1A8707new.png",
     description: ""
   },
 ];
