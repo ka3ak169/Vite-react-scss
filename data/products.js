bd9a8df4002caa19a192adf669d83ed5c06f6716
Aleksey Kazakov <ka3akne@mail.ru>
2025-02-18 00:14:35 +0400

    Updates

--- data/products.js
+++ data/products.js
@@ -1,66 +1,54 @@
-export const products = [
-  {
-    pic: "/public/images/new_image/O1A8779.jpg",
-    description: "A classic strong spirit with a distinctive juniper aroma, made by distilling grain alcohol with the addition of juniper berries and various botanical components such as coriander, citrus peel, and cardamom."
-  },
-  {
-    pic: "images/bergamot.png",
-    description: "An exquisite Italian liqueur with a bright citrusy aroma and flavor of bergamot, made by infusing bergamot peel in neutral spirits with the addition of sugar and sometimes other citrus fruits or herbs."
-  },
-  {
-    pic: "images/elderflower.png",
-    description: "A fragrant liqueur with a rich flavor of black elderberries, made by infusing ripe elderberries in neutral spirits with the addition of sugar and sometimes other ingredients such as citrus or spices"
-  },
-  {
-    pic: "images/triplesec1.png",
-    description: "A sweet, clear liqueur with a pronounced orange flavor and aroma, made by infusing dried orange peel in neutral spirits with the addition of sugar and sometimes other citrus fruits."
-  },
-  {
-    pic: "images/feijoa1.png",
-    description: "A unique strong spirit with an exotic fruity aroma and flavor of feijoa, made by distilling fermented feijoa juice and sometimes with the addition of other fruits or botanical components."
-  },
-  {
-    pic: "images/dill1.png",
-    description: "An aromatic strong spirit with a distinctive refreshing taste of dill, made by distilling grain alcohol with the addition of dill seeds and sometimes other herbs or spices."
-  }
-];
+// export const products = [
+//   {
+//     pic: "/images/gin.jpg",
+//     description: "A classic strong spirit with a distinctive juniper aroma, made by distilling grain alcohol with the addition of juniper berries and various botanical components such as coriander, citrus peel, and cardamom."
+//   },
+//   {
+//     pic: "images/bergamot.png",
+//     description: "An exquisite Italian liqueur with a bright citrusy aroma and flavor of bergamot, made by infusing bergamot peel in neutral spirits with the addition of sugar and sometimes other citrus fruits or herbs."
+//   },
+//   {
+//     pic: "images/elderflower.png",
+//     description: "A fragrant liqueur with a rich flavor of black elderberries, made by infusing ripe elderberries in neutral spirits with the addition of sugar and sometimes other ingredients such as citrus or spices"
+//   },
+//   {
+//     pic: "images/triplesec1.png",
+//     description: "A sweet, clear liqueur with a pronounced orange flavor and aroma, made by infusing dried orange peel in neutral spirits with the addition of sugar and sometimes other citrus fruits."
+//   },
+//   {
+//     pic: "images/feijoa1.png",
+//     description: "A unique strong spirit with an exotic fruity aroma and flavor of feijoa, made by distilling fermented feijoa juice and sometimes with the addition of other fruits or botanical components."
+//   },
+//   {
+//     pic: "images/dill1.png",
+//     description: "An aromatic strong spirit with a distinctive refreshing taste of dill, made by distilling grain alcohol with the addition of dill seeds and sometimes other herbs or spices."
+//   }
+// ];
 
 export const slides = [
   {
-    image: "/images/new_image/O1A8779.jpg",
+    image: "/images/gin.jpg",
     description: "A classic strong spirit with a distinctive juniper aroma, made by distilling grain alcohol with the addition of juniper berries and various botanical components such as coriander, citrus peel, and cardamom."
   },
   {
-    image: "/images/new_image/O1A8838new.png",
+    image: "/images/bergamot.jpg",
     description: "An exquisite Italian liqueur with a bright citrusy aroma and flavor of bergamot, made by infusing bergamot peel in neutral spirits with the addition of sugar and sometimes other citrus fruits or herbs."
   },
   {
-    image: "/images/new_image/O1A8701new.png",
-    description: "A fragrant liqueur with a rich flavor of black elderberries, made by infusing ripe elderberries in neutral spirits with the addition of sugar and sometimes other ingredients such as citrus or spices"
-  },
-  {
-    image: "/images/new_image/O1A8939new.jpg",
+    image: "/images/tripleSec.jpg",
     description: "A sweet, clear liqueur with a pronounced orange flavor and aroma, made by infusing dried orange peel in neutral spirits with the addition of sugar and sometimes other citrus fruits."
   },
   {
-    image: "/images/new_image/O1A8836new.png",
+    image: "/images/feijoa.jpg",
     description: "A unique strong spirit with an exotic fruity aroma and flavor of feijoa, made by distilling fermented feijoa juice and sometimes with the addition of other fruits or botanical components."
   },
   {
-    image: "/images/new_image/O1A8824new.png",
+    image: "/images/herbal.jpg",
     description: "A rich and aromatic herbal liqueur, bursting with complex flavors and a velvety texture."
   },
   {
-    image: "/images/new_image/O1A8795 3.jpg",
-    description: ""
-  },
-  {
-    image: "/images/new_image/O1A8864.jpg",
-    description: ""
-  },
-  {
-    image: "/images/new_image/O1A8707new.png",
-    description: ""
+    image: "/images/liquors.jpg",
+    description: "A rich and aromatic herbal liqueur, bursting with complex flavors and a velvety texture."
   },
 ];
 
