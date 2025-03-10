f6e9446e2df463a65bc7b22d69947052ba093155
Aleksey Kazakov <ka3akne@mail.ru>
2025-03-10 20:26:17 +0400

    Updates

--- data/products.js
+++ data/products.js
@@ -28,29 +28,36 @@
 export const slides = [
   {
     image: "/images/gin.jpg",
-    description: "A classic strong spirit with a distinctive juniper aroma, made by distilling grain alcohol with the addition of juniper berries and various botanical components such as coriander, citrus peel, and cardamom."
+    description: "Our gin is the art of cold distillation, a process that retains all the strength and freshness of nature in every drop. The aromas of juniper, the sweetness of licorice, the warmth of coriander, the tenderness of cinnamon and lavender, fennel, angelica and the magic of calamus, the spice of cardamom merge in a unique combination that personifies the soul of drink.",
+    link: "/gin"
   },
   {
     image: "/images/bergamot.jpg",
-    description: "An exquisite Italian liqueur with a bright citrusy aroma and flavor of bergamot, made by infusing bergamot peel in neutral spirits with the addition of sugar and sometimes other citrus fruits or herbs."
+    description: "Bergamot liqueur is an exquisite drink created from natural ingredients using cold distillation. Its unique taste is revealed thanks to carefully selected Ingredients. Every drop of this drink carries a unique pleasure and reveals the soul of drinks.",
+    link: "/bergamot"
   },
   {
     image: "/images/tripleSec.jpg",
-    description: "A sweet, clear liqueur with a pronounced orange flavor and aroma, made by infusing dried orange peel in neutral spirits with the addition of sugar and sometimes other citrus fruits."
+    description: "Triple Sec liqueur is an exquisite combination of tradition and quality, created by cold distillation and the use of selected ingredients. Its bright citrus taste and aroma evoke warm, sunny days. Every sip of this liqueur is an immersion in the rich world of citrus nuances, giving the drink freshness and brightness with the soul of drinks.",
+    link: "/triple-sec"
   },
   {
     image: "/images/feijoa.jpg",
-    description: "A unique strong spirit with an exotic fruity aroma and flavor of feijoa, made by distilling fermented feijoa juice and sometimes with the addition of other fruits or botanical components."
+    description: "Our feijoa distillate is an exceptionally pure aroma made using cold distillation technology, each note carefully expresses the essence of nature, creating a unique taste experience. It's not just a drink; this is the soul of the drinks.",
+    link: "/feijoa"
   },
   {
     image: "/images/herbal.jpg",
-    description: "A rich and aromatic herbal liqueur, bursting with complex flavors and a velvety texture."
+    description: "A complex and sophisticated bouquet that combines intense herbal notes, delicate floral shades and warm spicy accents. Balance and a deep aftertaste fill the sip with harmonious aromas of herbs, citrus and spices. The soul of drink for connoisseurs of exquisite taste.",
+    link: "/herbal"
   },
   {
     image: "/images/liquors.jpg",
-    description: "A rich and aromatic herbal liqueur, bursting with complex flavors and a velvety texture."
-  },
+    description: "",
+    link: "/"
+  }
 ];
+
 
 export const youtubeLink = [
   'https://youtu.be/jfkouSD9HmY?si=ly5SYBT7Bjp8X4L8'
